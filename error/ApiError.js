08432af709ec532e3fe3eac24c342c38e23c3b67
Harsh Raj <saketharshraj@gmail.com>
2022-02-17 01:16:49 +0530

    Added few error types

--- error/ApiError.js
+++ error/ApiError.js
@@ -15,6 +15,22 @@ class ApiError {
   static forbidden(message) {
     return new ApiError(message, 403);
   }
+
+  static conflict(message) {
+    return new ApiError(message, 409);
+  }
+
+  static notFound(message) {
+    return new ApiError(message, 404);
+  }
+
+  static failedDependency(message) {
+    return new ApiError(message, 424);
+  }
+
+  static internalServerError(message) {
+    return new ApiError(message, 500);
+  }
 }
 
 module.exports = ApiError;
