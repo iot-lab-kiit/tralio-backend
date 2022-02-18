aadb15c66ee3a1cac243917b50467e100fb17b21
Harsh Raj <saketharshraj@gmail.com>
2022-02-18 12:27:10 +0530

    Minor changes due to added middleware

--- controllers/users/login.js
+++ controllers/users/login.js
@@ -1,35 +1,39 @@
 const bcrypt = require("bcrypt");
 const User = require("../../models/user/userSchema");
 const createJWT = require("../../helpers/createJWT");
+const ApiError = require("../../error/ApiError");
 
-const login = async (req, res) => {
+const login = async (req, res, next) => {
   const { username, userEmail, userPassword } = req.body;
 
-   // Todo : Need error handling for mongo query
+  // Todo : Need error handling for mongo query
   const foundUser = await User.findOne({ username: username });
   if (!foundUser) foundUser = await User.findOne({ userEmail: userEmail });
 
   if (!foundUser) {
-    res.status(404).json({ message: "User not found" });
-  } else {
-
-    // Todo : Need error handling for bcrypt
-    const validPassword = await bcrypt.compare(
-      userPassword,
-      foundUser.userPassword
-    );
-    if (validPassword) {
-      const token = createJWT(foundUser)
-      res.status(200).json({ message: "User AUthentication Successfull", user: foundUser, token: token});
-    } else {
-      res.status(400).json({ error: "Invalid Password" });
-    }
+    next(ApiError.notFound("User not found"));
+    return;
   }
+
+  // Todo : Need error handling for bcrypt
+  const validPassword = await bcrypt.compare(
+    userPassword,
+    foundUser.userPassword
+  );
+
+  if (!validPassword) {
+    next(ApiError.badRequest("Invalid Password"));
+    return;
+  }
+
+  const token = createJWT(foundUser);
+  res.status(200).json({
+    message: "User AUthentication Successfull",
+    user: foundUser,
+    token: token,
+  });
+
+  next();
 };
 
-const refreshJWT = async(req, res) => {
-  const token = createJWT(req.body)
-  res.status(200).json({token:token, user:req.body})
-}
-
-module.exports = { login, refreshJWT };
+module.exports = { login};
