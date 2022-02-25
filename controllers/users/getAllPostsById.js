4e0c8acb83f2647e8455980584144cc4213d1ab8
Cation-Coder6 <72982245+Cation-Coder6@users.noreply.github.com>
2022-02-25 13:41:29 +0530

    wrapped function in a try catch block

--- controllers/users/getAllPostsById.js
+++ controllers/users/getAllPostsById.js
@@ -2,14 +2,18 @@ const ApiError = require("../../error/ApiError");
 const Post = require("../../models/post/postSchema");
 
 const getAllPosts = async (req, res, next) => {
-  const userId = req.params.id;
+  try {
+    const userId = req.params.id;
 
-  const posts = await Post.find({ postedBy: userId });
-  if (!posts) {
-    next(ApiError.notFound("No Posts Found"));
+    const posts = await Post.find({ postedBy: userId });
+    if (!posts) {
+      next(ApiError.notFound("No Posts Found"));
+    }
+
+    res.status(200).json({ posts });
+  } catch (err) {
+    next(ApiError.internalServerError("Error in searching for user"));
   }
-
-  res.status(200).json({ posts });
 };
 
 module.exports = getAllPosts;
