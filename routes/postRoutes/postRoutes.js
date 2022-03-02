82eab6c7c8fd9b776f16372b7fea159136595709
Aayushman03 <aayushmanpratap333@gmail.com>
2022-03-02 19:03:32 +0530

    added edit post function

--- routes/postRoutes/postRoutes.js
+++ routes/postRoutes/postRoutes.js
@@ -7,5 +7,6 @@ postRoutes.post('/create', postController.createPost);
 postRoutes.get('/all-posts', postController.getAllPost);
 postRoutes.delete('/delete/(:id)', postController.deletePost);
 postRoutes.get('/:id',postController.postById );
+postRoutes.patch('/edit-post/:id', postController.editPost.editPost);
 
 module.exports = postRoutes;
