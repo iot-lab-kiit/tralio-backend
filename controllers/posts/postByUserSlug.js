a5447af3a5292dd0708f2d5672a08a1dec5a8349
Harsh Raj <saketharshraj@gmail.com>
2022-03-03 16:21:59 +0530

    postByUserSlug setup 2

--- controllers/posts/postByUserSlug.js
+++ controllers/posts/postByUserSlug.js
@@ -1,1 +1,22 @@
 // Write your code for the postByUserSlug controller here.
+// Check for any problem in the code and fix it.
+const Post = require("../../models/post/postSchema");
+const User = require("../../models/user/userSchema");
+const ApiError = require("../../error/ApiError");
+
+const postByUserSlug = async (req, res, next) => {
+    try {
+        const username = req.params.username;
+        const slug = req.params.slug;
+        const posts = await Post.find({ postedBy: userId, slug: slug });
+        if (!posts) {
+            next(ApiError.notFound("No Posts Found"));
+        }
+        res.status(200).json({ posts });
+    } catch (err) {
+        next(ApiError.internalServerError("Error in searching for user"));
+    }
+};
+
+module.exports = postByUserSlug;
+
