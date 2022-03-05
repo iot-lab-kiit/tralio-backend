949e378b27c1b5e2e60e8170ca321d1692287d49
Shreya <www.shreyaagarwal.com@gmail.com>
2022-03-05 15:18:09 +0530

    refresh-jwt created

--- helpers/createJWT.js
+++ helpers/createJWT.js
@@ -1,10 +1,23 @@
 const jwt = require("jsonwebtoken");
+const redis_client = require('../redis_connect');
 
 const createJWT = (user) => {
   return jwt.sign(
     { _id: user._id, email: user.email },
     process.env.SECRET_KEY,
-    { expiresIn: process.env.JWT_LIFETIME }
+    { expiresIn: process.env.JWT_ACCESS_LIFETIME }
   );
 };
-module.exports = createJWT;
+
+const createRefreshJWT = (user) => {
+  const refresh_token =  jwt.sign(
+    { _id: user._id, email: user.email },
+    process.env.REFRESH_SECRET_KEY,
+    { expiresIn: process.env.JWT_REFRESH_LIFETIME }
+  );
+  
+  // If value exist it will update and if not then will add it
+  redis_client.set(user._id.toString(), refresh_token);
+  return refresh_token;
+};
+module.exports = { createJWT, createRefreshJWT };
