d22184944765b3aa3565229e4de52210531f2ed9
Shreya <www.shreyaagarwal.com@gmail.com>
2022-03-05 15:19:01 +0530

    refresh token verification added

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,8 +1,8 @@
-const user = require("../models/user/userSchema");
+// const user = require("../models/user/userSchema");
 const jwt = require("jsonwebtoken");
-const { rawListeners } = require("../models/user/userSchema");
+const redis_client = require('../redis_connect');
 
-const authenticator = (req, res, next) => {
+const verifyToken = (req, res, next) => {
   const bearerHeader = req.headers["authorization"];
   if (!bearerHeader || !bearerHeader.startsWith("Bearer")) {
     console.log("NO JWT TOKEN");
@@ -11,7 +11,7 @@ const authenticator = (req, res, next) => {
     try {
       const payload = jwt.verify(bearerToken, process.env.SECRET_KEY);
       req.user = {
-        userId: payload._id,
+        _id: payload._id,
         email: payload.email,
       };
       next();
@@ -20,3 +20,28 @@ const authenticator = (req, res, next) => {
     }
   }
 };
+
+const verifyRefreshToken = async (req,res,next)=>{
+  const refresh_token = req.body.token;
+  if(!refresh_token){
+    return res.status(401).json({message : "Invalid request"})
+  }
+  try {
+    const payload = jwt.verify(refresh_token, process.env.REFRESH_SECRET_KEY)
+      req.user =  {
+        _id: payload._id,
+        email: payload.email,
+      };
+      const userId = payload._id;
+      redis_client.get(userId, (err, result)=>{
+        if(!result) return res.status(401).json({message : "Invalid request. Token is not in cache"});
+        if(result !== refresh_token)
+          return res.status(401).json({message : "Invalid request. Token is not same"});
+        next();
+    })
+  } catch (error) {
+    return res.status(401).json({message :  `Session is invalid`})
+  }
+}
+
+module.exports = {verifyToken, verifyRefreshToken};
