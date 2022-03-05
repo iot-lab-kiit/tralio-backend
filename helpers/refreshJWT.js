d7b0726cb8058736e7aed7c83c10d02b726ac17d
Shreya <www.shreyaagarwal.com@gmail.com>
2022-03-05 15:18:33 +0530

    refresh-jwt implemented

--- helpers/refreshJWT.js
+++ helpers/refreshJWT.js
@@ -1,8 +1,19 @@
-const createJWT = require("./createJWT");
+const { createJWT, createRefreshJWT } = require("./createJWT");
 
-const refreshJWT = async (req, res) => {
-  const token = createJWT(req.body);
-  res.status(200).json({ token: token, user: req.body });
+const refreshJWT = async (req, res, next) => {
+  try {
+    // generate new access and refresh token
+    const user = req.user;
+    const access_token = createJWT(user);
+    const refresh_token = createRefreshJWT(user);
+    res.status(200).json({
+      message: "successfully refreshed token",
+      token: { access_token, refresh_token },
+    });
+    next();
+  } catch (error) {
+    return res.status(500).json({ message: "error in refreshing token" });
+  }
 };
 
 module.exports = refreshJWT;
