45ff99d99eb06c9e46d2f63c5baa5775d976b25f
Cation-Coder6 <72982245+Cation-Coder6@users.noreply.github.com>
2022-03-15 16:30:53 +0530

    edit post functionality added

--- routes/postRoutes/postRoutes.js
+++ routes/postRoutes/postRoutes.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const postController = require('../../controllers/posts/postController');
+const express = require("express");
+const postController = require("../../controllers/posts/postController");
 const postRoutes = express.Router();
 
-
-postRoutes.post('/create', postController.createPost);
-postRoutes.get('/all-posts', postController.getAllPost);
-postRoutes.delete('/:id', postController.deletePost);
-postRoutes.get('id/:id',postController.postById );
+postRoutes.post("/create", postController.createPost);
+postRoutes.get("/all-posts", postController.getAllPost);
+postRoutes.delete("/:id", postController.deletePost);
+postRoutes.patch("/:id/edit-post", postController.editPost);
+postRoutes.get("id/:id", postController.postById);
 
 module.exports = postRoutes;
