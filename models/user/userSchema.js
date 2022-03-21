44d51053f4f25ba993e6019ab081d683eba38a41
Cation-Coder6 <72982245+Cation-Coder6@users.noreply.github.com>
2022-03-21 17:39:57 +0530

    Merge branch 'main' of https://github.com/iot-lab-kiit/tralio-backend

--- models/user/userSchema.js
+++ models/user/userSchema.js
@@ -1,41 +1,60 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
     required: [true, "Username Missing"],
-    index: {unique: true, dropDups: true}
+    index: { unique: true, dropDups: true },
   },
+
   firstname: {
     type: String,
     required: [true, "First Name Missing"],
   },
+
   lastname: {
     type: String,
     required: [true, "Last Name Missing"],
   },
+
   userEmail: {
     type: String,
     required: [true, "Email Missing"],
-    index: {unique: true, dropDups: true}
+    index: { unique: true, dropDups: true },
   },
+
   userPassword: {
     type: String,
     required: [true, "Password Misssing"],
     minlength: 6,
   },
+
   userDOB: {
     type: String,
   },
+
   userGender: {
     type: String,
     required: [true, "Gender Missing"],
   },
+
   userCountry: {
     type: String,
   },
-  
 });
+
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+  const salt = await bcrypt.genSalt(parseInt(process.env.SALT_ROUNDS));
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  const isMatch = await bcrypt.compare(candidatePassword, this.password);
+  return isMatch;
+};
 
 const User = mongoose.model("users", userSchema);
 
