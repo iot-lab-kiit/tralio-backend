8963a00e5060da22b68b1cd3c3a35a07b9e02d81
Cation-Coder6 <72982245+Cation-Coder6@users.noreply.github.com>
2022-03-25 23:07:06 +0530

    create Portfolio controller completed

--- controllers/portfolio/createPortfolio.js
+++ controllers/portfolio/createPortfolio.js
@@ -2,7 +2,23 @@ const Portfolio = require("../../models/portfolio/portfolioSchema");
 const ApiError = require("../../error/ApiError");
 
 const create = async (req, res) => {
-  res.send("Create Controller");
+  const portfolioData = req.body;
+
+  if (!portfolioData) {
+    next(ApiError.badRequest("Please Enter Details"));
+  }
+
+  if (!portfolioData.name || !portfolioData.email) {
+    next(ApiError.badRequest("Please Enter Data for All Fields"));
+  }
+
+  const portfolio = await Portfolio.create(portfolioData);
+
+  if (!portfolio) {
+    next(ApiError.internalServerError("Database Error"));
+  }
+
+  res.status(201).json(portfolio);
 };
 
 module.exports = create;
