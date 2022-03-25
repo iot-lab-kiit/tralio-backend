97e3821ef9b5760e9e06abef41636f4ec2a100b6
Cation-Coder6 <72982245+Cation-Coder6@users.noreply.github.com>
2022-03-26 00:15:16 +0530

    delete portfolio controller completed

--- controllers/portfolio/deletePortfolio.js
+++ controllers/portfolio/deletePortfolio.js
@@ -2,7 +2,19 @@ const Portfolio = require("../../models/portfolio/portfolioSchema");
 const ApiError = require("../../error/ApiError");
 
 const deletePortfolio = async (req, res, next) => {
-  res.send("Delete Portfolio Added");
+  try {
+    const portfolioId = req.params.id;
+
+    const portfolio = await Portfolio.findByIdAndDelete({ _id: portfolioId });
+    if (!portfolio) {
+      next(ApiError.badRequest(`No Post with ID ${portfolioId} Found`));
+      return;
+    }
+
+    return res.status(200).send(portfolio);
+  } catch (error) {
+    return next(ApiError.internalServerError(error.message));
+  }
 };
 
 module.exports = deletePortfolio;
