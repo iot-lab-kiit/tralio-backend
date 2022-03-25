e8e65b5b098d40b837d01564551298126c12c135
Cation-Coder6 <72982245+Cation-Coder6@users.noreply.github.com>
2022-03-25 23:23:41 +0530

    get all portfolio function completed

--- controllers/portfolio/getAllPortfolio.js
+++ controllers/portfolio/getAllPortfolio.js
@@ -2,7 +2,17 @@ const Portfolio = require("../../models/portfolio/portfolioSchema");
 const ApiError = require("../../error/ApiError");
 
 const getAllPortfolio = async (req, res) => {
-  res.send("Get All Portfolio");
+  var portfolios;
+  try {
+    portfolios = await Portfolio.find({});
+  } catch (err) {
+    next(ApiError.internalServerError("Database Error"));
+  }
+  if (!portfolios) {
+    next(ApiError.notFound("Portfolios not found"));
+  }
+
+  res.status(200).json({ portfolios, count: portfolios.length });
 };
 
 module.exports = getAllPortfolio;
