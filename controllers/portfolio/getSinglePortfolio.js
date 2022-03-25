8a8825ff724305d8707bb18a709b50d31341d9f2
Cation-Coder6 <72982245+Cation-Coder6@users.noreply.github.com>
2022-03-25 23:41:20 +0530

    get single portfolio function completed

--- controllers/portfolio/getSinglePortfolio.js
+++ controllers/portfolio/getSinglePortfolio.js
@@ -1,8 +1,21 @@
 const Portfolio = require("../../models/portfolio/portfolioSchema");
 const ApiError = require("../../error/ApiError");
 
-const getSinglePortfolio = async (req, res) => {
-  res.send("Get Single Portfolio Added");
+const getSinglePortfolio = async (req, res, next) => {
+  try {
+    const portfolioId = req.params.id;
+
+    const portfolio = await Portfolio.findOne({ _id: portfolioId });
+
+    if (!portfolio) {
+      next(ApiError.notFound("Portfolio with current id not found"));
+      return;
+    }
+    res.status(200).json({ portfolio: portfolio });
+  } catch (err) {
+    next(ApiError.internalServerError("Error in searching for user"));
+    return;
+  }
 };
 
 module.exports = getSinglePortfolio;
