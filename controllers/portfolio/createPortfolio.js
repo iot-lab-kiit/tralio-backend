defb486c69a94b514d16db681496a63f8e17ca58
Cation-Coder6 <72982245+Cation-Coder6@users.noreply.github.com>
2022-03-25 23:39:28 +0530

    changes in error handling

--- controllers/portfolio/createPortfolio.js
+++ controllers/portfolio/createPortfolio.js
@@ -1,21 +1,24 @@
 const Portfolio = require("../../models/portfolio/portfolioSchema");
 const ApiError = require("../../error/ApiError");
 
-const create = async (req, res) => {
+const create = async (req, res, next) => {
   const portfolioData = req.body;
 
   if (!portfolioData) {
     next(ApiError.badRequest("Please Enter Details"));
+    return;
   }
 
   if (!portfolioData.name || !portfolioData.email) {
     next(ApiError.badRequest("Please Enter Data for All Fields"));
+    return;
   }
   var portfolio;
   try {
     portfolio = await Portfolio.create(portfolioData);
   } catch (err) {
     next(ApiError.internalServerError("Database Error"));
+    return;
   }
 
   res.status(201).json(portfolio);
