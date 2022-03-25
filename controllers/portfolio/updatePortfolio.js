2e230597d2061f7ef42af7cc018139761ec1d9e7
Cation-Coder6 <72982245+Cation-Coder6@users.noreply.github.com>
2022-03-26 00:02:34 +0530

    update portfolio function completed

--- controllers/portfolio/updatePortfolio.js
+++ controllers/portfolio/updatePortfolio.js
@@ -2,7 +2,27 @@ const Portfolio = require("../../models/portfolio/portfolioSchema");
 const ApiError = require("../../error/ApiError");
 
 const updatePortfolio = async (req, res, next) => {
-  res.send("Update Portfolio Added");
+  const { id: portfolioId } = req.params;
+  try {
+    const portfolio = await Portfolio.findOneAndUpdate(
+      { _id: portfolioId },
+      req.body,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+
+    if (!portfolio) {
+      next(ApiError.notFound(`No portfolio with id : ${portfolioId}`));
+      return;
+    }
+
+    res.status(200).json({ portfolio });
+  } catch (err) {
+    next(ApiError.internalServerError("Error in searching for portfolio"));
+    return;
+  }
 };
 
 module.exports = updatePortfolio;
