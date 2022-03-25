fa2ee379a8e0caa6064ee8015b0111b41c1c6d8e
Cation-Coder6 <72982245+Cation-Coder6@users.noreply.github.com>
2022-03-25 23:39:52 +0530

    changes in error handlling

--- controllers/portfolio/getAllPortfolio.js
+++ controllers/portfolio/getAllPortfolio.js
@@ -1,15 +1,17 @@
 const Portfolio = require("../../models/portfolio/portfolioSchema");
 const ApiError = require("../../error/ApiError");
 
-const getAllPortfolio = async (req, res) => {
+const getAllPortfolio = async (req, res, next) => {
   var portfolios;
   try {
     portfolios = await Portfolio.find({});
   } catch (err) {
     next(ApiError.internalServerError("Database Error"));
+    return;
   }
   if (!portfolios) {
     next(ApiError.notFound("Portfolios not found"));
+    return;
   }
 
   res.status(200).json({ portfolios, count: portfolios.length });
