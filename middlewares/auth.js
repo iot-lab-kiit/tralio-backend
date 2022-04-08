8c869bb8d67e3e22d334e2d8806384c75ae21f0d
Harsh Raj <saketharshraj@gmail.com>
2022-04-08 16:01:29 +0530

    Register and Login working properly and also sending proper cookies

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,15 +1,13 @@
-const user = require("../models/user/userSchema");
 const jwt = require("jsonwebtoken");
-const { rawListeners } = require("../models/user/userSchema");
 
-const authenticator = (req, res, next) => {
+const auth = (req, res, next) => {
   const bearerHeader = req.headers["authorization"];
   if (!bearerHeader || !bearerHeader.startsWith("Bearer")) {
     console.log("NO JWT TOKEN");
   } else {
     const bearerToken = bearerHeader.split(" ")[1];
     try {
-      const payload = jwt.verify(bearerToken, process.env.SECRET_KEY);
+      const payload = jwt.verify(bearerToken, process.env.JWT_KEY);
       req.user = {
         userId: payload._id,
         email: payload.email,
@@ -20,3 +18,5 @@ const authenticator = (req, res, next) => {
     }
   }
 };
+
+module.exports = auth;
--- routes/userRoutes/userRoutes.js
+++ routes/userRoutes/userRoutes.js
@@ -3,9 +3,9 @@ const routes = express.Router();
 const refreshJWT = require("../../helpers/refreshJWT");
 const UserController = require("../../controllers/users/userController");
 const validateCreateUserPayload = require("../../middlewares/userMiddlewares/validateCreateUserPayload");
+const verifyOtp = require("../../middlewares/userMiddlewares/verifyOtp");
 
-routes.post("/register", validateCreateUserPayload, UserController.register);
-
+routes.post("/register", [validateCreateUserPayload, verifyOtp], UserController.register);
 routes.post("/login", UserController.login);
 routes.post("/refresh-token", refreshJWT);
 routes.get("/user-by-id/:id", UserController.userById);
